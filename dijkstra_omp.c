4eda7d32e3aecc3e905f9ecd4a713e8251bf11a7
scott50301@gmail.com <scott50301@gmail.com>
2020-05-19 16:43:33 -0700

    print thread parameter

--- dijkstra_omp.c
+++ dijkstra_omp.c
@@ -82,7 +82,7 @@ dijkstra(
   m[s] = 1;
   min.u = -1; /* avoid compiler warning */
   omp_set_num_threads(threads);
-  printf("thread¡G%s\n",threads);
+  printf("thread¡G%o\n",threads);
 #pragma omp parallel
   {
 #pragma omp for
