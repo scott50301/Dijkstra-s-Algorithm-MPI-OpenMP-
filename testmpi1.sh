d8a7e6b28d409cc062e75c607d0c616e6512d1d6
scott50301@gmail.com <scott50301@gmail.com>
2020-05-26 22:31:50 -0700

    mpi sh

--- testmpi1.sh
+++ testmpi1.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
+echo 1
 mpirun -n 1 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 1 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 1 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
 mpirun -n 1 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
 mpirun -n 1 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+echo 2
 mpirun -n 2 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 2 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 2 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
 mpirun -n 2 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
 mpirun -n 2 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+echo 4
 mpirun -n 4 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 4 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 4 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
 mpirun -n 4 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 4 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+mpirun -n 4 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.ouecho
+echo 8
 mpirun -n 8 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 8 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 8 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
 mpirun -n 8 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
 mpirun -n 8 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+echo 16
 mpirun -n 16 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 16 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 16 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
 mpirun -n 16 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
 mpirun -n 16 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+echo 28
 mpirun -n 28 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
 mpirun -n 28 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
 mpirun -n 28 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
