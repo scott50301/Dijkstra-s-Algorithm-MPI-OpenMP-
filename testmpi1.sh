80504f5e3334bd5f04232160cd4e67d0baa391f4
scott50301@gmail.com <scott50301@gmail.com>
2020-05-28 18:58:23 -0700

    add sh

--- testmpi1.sh
+++ testmpi1.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
-echo 1
-mpirun -n 1 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 1 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 1 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 1 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 1 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
-echo 2
-mpirun -n 2 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 2 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 2 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 2 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 2 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
-echo 4
-mpirun -n 4 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 4 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 4 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 4 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 4 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.ouecho
-echo 8
-mpirun -n 8 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 8 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 8 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 8 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 8 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
-echo 16
-mpirun -n 16 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 16 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 16 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 16 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 16 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
-echo 28
-mpirun -n 28 ./dijkstra_mpi ./data/100.graph 1 test_mpi.out
-mpirun -n 28 ./dijkstra_mpi ./data/500.graph 1 test_mpi.out
-mpirun -n 28 ./dijkstra_mpi ./data/1000.graph 1 test_mpi.out
-mpirun -n 28 ./dijkstra_mpi ./data/5000.graph 1 test_mpi.out
-mpirun -n 28 ./dijkstra_mpi ./data/10000.graph 1 test_mpi.out
+#
+#SBATCH --job-name=dijkstra_mpi
+#SBATCH --nodes=3
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=84
+#SBATCH --mem-per-cpu=16G
+#SBATCH --output=dijkstra_mpi-%j.out 
+#SBATCH --time=23:59:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sjhou@scu.edu #
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+log_file_name=dijkstra_mpi-$current_time.log
+file_path=/WAVE/users/unix/sjhou/Dijkstra-s-Algorithm-MPI-OpenMP- 
+module load OpenMPI/3.1.4-GCC-8.3.0
+
+for size in 100 500 1000 5000 10000; do
+    for thread_num in 1 2 4 8 16 28; do
+    # for k in seq 1 20; do   
+        for k in seq 1 5; do
+            source=$RANDOM
+            let "source %= $size"
+            mpirun -n $thread_num --map-by node $file_path/dijkstra_mpi "$file_path/data/${size}.graph" $source "$file_path/data/${size}_serial.out" >> "$file_path/logs/$log_file_name"
+        done 
+    done
+done
