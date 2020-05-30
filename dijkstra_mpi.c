723932d6c3d7021b88233390fa69e691011de852
scott50301@gmail.com <scott50301@gmail.com>
2020-05-29 17:24:07 -0700

    change mpi

--- dijkstra_mpi.c
+++ dijkstra_mpi.c
@@ -22,36 +22,36 @@
 #define SEND_DISPLS_TAG 1
 #define SEND_ELEMNTS_TAG 2
 #define WEIGHT_TAG 3
-#define SEND_COUNTS_TAG 4
+#define COUNTS_TAG 4
 #define SEND_RESULT_TAG 5
 
-static void cal(int ** displs, int ** elements, int processors, int size){
+static void cal(int ** displs, int ** elements, int processes, int size){
     int local, reminder;
-    if (size >= processors){
-        local = size / processors;
-        reminder = size % processors;
+    if (size >= processes){
+        local = size / processes;
+        reminder = size % processes;
 
         //calculate numbers to send to each node.
-        *elements = malloc(processors * sizeof(**elements));
-        for (int i = 0; i < processors; i++) {
+        *elements = malloc(processes * sizeof(**elements));
+        for (int i = 0; i < processes; i++) {
             *(*elements + i) = local;
         }
-        *(*elements + processors - 1) += reminder;
+        *(*elements + processes - 1) += reminder;
         
     } else {//If there are more rows than available nodes, separate into almost equal pieces.
         local = 1;
-        *elements = malloc(processors * sizeof(**elements));
+        *elements = malloc(processes * sizeof(**elements));
         int i = 0;
         for (; i < size; i++) {
             *(*elements + i) = local;
         }
-        for (; i < processors; i++) {
+        for (; i < processes; i++) {
             *(*elements + i) = 0;
         }
     }
     //calculate offset that points from send buffer(vals).
-    *displs = malloc(processors * sizeof(**displs));
-    for (int i = 0; i < processors; i++) {
+    *displs = malloc(processes * sizeof(**displs));
+    for (int i = 0; i < processes; i++) {
         *(*displs + i) = 0;
         for (int j = 0; j < i; j++) {
             *(*displs + i) += *(*elements + j);
@@ -63,7 +63,7 @@ load(
     char const *const filename,
     int *const np,
     float **const ap, 
-    int processors, 
+    int processes,
     int ** displs, 
     int ** elements,
     int rank
@@ -84,7 +84,7 @@ load(
         assert(1 == ret);
 
         //Calculate how many rows each node will hold. And their offsets(displs).
-        cal(displs, elements, processors, n);
+        cal(displs, elements, processes, n);
 
         /* allocate memory for local values */
         a = malloc(n * *(*elements) * sizeof(*a));
@@ -94,20 +94,20 @@ load(
         }
         *ap = a;
 
-        for (i = 1; i < processors; ++i) {
+        for (i = 1; i < processes; ++i) {
             a = malloc(n * *(*elements + i) * sizeof(*a));
             //Send just collected info to that node
             MPI_Send(&n, 1, MPI_INTEGER, i, SEND_NUM_TAG, MPI_COMM_WORLD);
 
-            MPI_Send(*displs, processors, MPI_INTEGER, i, SEND_DISPLS_TAG, MPI_COMM_WORLD);
-            MPI_Send(*elements, processors, MPI_INTEGER, i, SEND_ELEMNTS_TAG, MPI_COMM_WORLD);
+            MPI_Send(*displs, processes, MPI_INTEGER, i, SEND_DISPLS_TAG, MPI_COMM_WORLD);
+            MPI_Send(*elements, processes, MPI_INTEGER, i, SEND_ELEMNTS_TAG, MPI_COMM_WORLD);
             //Read file
             for (j = 0; j < *(*elements + i) * n; ++j) {
                 ret = fscanf(fp, "%f", &a[j]);
                 assert(1 == ret);
             }
             //printf("%d. %d info read\n", i, j);//TODO debug use only
-            MPI_Send(&j, 1, MPI_INTEGER, i, SEND_COUNTS_TAG, MPI_COMM_WORLD);
+            MPI_Send(&j, 1, MPI_INTEGER, i, COUNTS_TAG, MPI_COMM_WORLD);
             MPI_Send(a, j, MPI_FLOAT, i, WEIGHT_TAG, MPI_COMM_WORLD);
             free(a);//Free memory after send.
         }
@@ -118,11 +118,11 @@ load(
     } else {//All nodes except MAIN NODE will receive piece(rows) of graph data.
         int count;
         MPI_Recv(&n, 1, MPI_INTEGER, MAIN_PROCESS, SEND_NUM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        *displs = malloc(processors * sizeof(**displs));
-        MPI_Recv(*displs, processors, MPI_INTEGER, MAIN_PROCESS, SEND_DISPLS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        *elements = malloc(processors * sizeof(**elements));
-        MPI_Recv(*elements, processors, MPI_INTEGER, MAIN_PROCESS, SEND_ELEMNTS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&count, 1, MPI_INTEGER, MAIN_PROCESS, SEND_COUNTS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        *displs = malloc(processes * sizeof(**displs));
+        MPI_Recv(*displs, processes, MPI_INTEGER, MAIN_PROCESS, SEND_DISPLS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        *elements = malloc(processes * sizeof(**elements));
+        MPI_Recv(*elements, processes, MPI_INTEGER, MAIN_PROCESS, SEND_ELEMNTS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&count, 1, MPI_INTEGER, MAIN_PROCESS, COUNTS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         a = malloc(count * sizeof(*a));
         MPI_Recv(a, count, MPI_FLOAT, MAIN_PROCESS, WEIGHT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         *ap = a;
@@ -135,14 +135,14 @@ load(
 
 static void
 dijkstra(
-    int const source,
+    int const s,
     int const n,
     float const *const a,
-    float **const result, 
+    float **const lp, 
     int rank, 
     int * displs, 
     int * elements,
-    int processors
+    int processes
 ) {
     int i, j, k, sourceNode = 0;
     struct float_int {
@@ -151,7 +151,7 @@ dijkstra(
     } min;
     char *m = NULL;
     float *l = NULL;
-    float * localRes= NULL;
+    float * ll= NULL;
 
 
     m = calloc(n, sizeof(*m));
@@ -160,11 +160,11 @@ dijkstra(
     l = malloc(n * sizeof(*l));
     assert(l);
 
-    localRes = malloc(n * sizeof(*l));
-    assert(localRes);
+    ll = malloc(n * sizeof(*l));
+    assert(ll);
 
-    for (i = 0; i < processors; i++){
-        if (source < displs[i]){
+    for (i = 0; i < processes; i++){
+        if (s < displs[i]){
             sourceNode = i - 1;
             break;
         }
@@ -173,27 +173,26 @@ dijkstra(
     //The initial result distance is what currently the distance between source to each vertex.
     if (rank == sourceNode) {//Copy and prepare for broad cast.
         for (i = 0; i < n; ++i) {
-            l[i] = a[i + n * (source - displs[sourceNode])];
+            l[i] = a[i + n * (s - displs[sourceNode])];
             
         }
     }
     MPI_Bcast(l, n, MPI_FLOAT, sourceNode, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    //We don't have to visit source vertex.
-    //Such that the distance from source to source is defined in the graph_matrix[source, source].
-    m[source] = 1;
+   
+    m[s] = 1;
     min.u = -1; /* avoid compiler warning */
 
-    //Iterate through each vertex.
+    //Iterate through each.
     for (i = 1; i < n; ++i) {
         min.distance = INFINITY;
-        //Find local minimum vertex in order to do relax operation(update min distance to each vertex that vertex connected with).
+
         for (j = 0; j < n; ++j) {
             if (!m[j] && l[j] < min.distance) {
                 min.distance = l[j];
                 min.u = j;
             }
-            localRes[j] = l[j];
+            ll[j] = l[j];
         }
        
         m[min.u] = 1;
@@ -201,18 +200,18 @@ dijkstra(
             if (m[j + displs[rank]]){
                 continue;
             }
-            if (a(j, min.u) + min.distance < localRes[j + displs[rank]]){
-                localRes[j + displs[rank]] = a(j, min.u) + min.distance;
+            if (a(j, min.u) + min.distance < ll[j + displs[rank]]){
+                ll[j + displs[rank]] = a(j, min.u) + min.distance;
             }
         }
 
-        MPI_Allreduce(localRes, l, n, MPI_FLOAT, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(ll, l, n, MPI_FLOAT, MPI_MIN, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
     }
 
     free(m);
 
-    *result = l;
+    *lp = l;
 }
 
 static void
@@ -245,7 +244,7 @@ print_numbers(
 
 int
 main(int argc, char **argv) {
-    int n, processors, rank;;
+    int n, npes, myrank;
     double ts, te;
     float *a = NULL, *result = NULL;
     int * displs = NULL, * elements = NULL;
@@ -255,19 +254,19 @@ main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, &processors);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    load(argv[1], &n, &a, processors, &displs, &elements, rank);
+    load(argv[1], &n, &a, npes, &displs, &elements, myrank);
 
     MPI_Barrier(MPI_COMM_WORLD);
     ts = MPI_Wtime();
-    dijkstra(atoi(argv[2]), n, a, &result, rank, displs, elements, processors);
+    dijkstra(atoi(argv[2]), n, a, &result, myrank, displs, elements, npes);
     te = MPI_Wtime();
 
     print_time((te - ts) / CLOCKS_PER_SEC);
-    if (rank == MAIN_PROCESS) {
+    if (myrank == MAIN_PROCESS) {
         print_time(te - ts);
         print_numbers(argv[3], n, result);
     }
